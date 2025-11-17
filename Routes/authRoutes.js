551bad4e54b93d48ec0158669cac68c0926835e7
HarmanGIT10 <harmanjeet112005@gmail.com>
2025-11-17 15:31:41 +0530

    Update authRoutes.js

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -9,7 +9,9 @@ const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
