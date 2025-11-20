bd055d56dfe83becd03ffbeeaf6c4f6e02c9a0fb
HarmanGIT10 <harmanjeet112005@gmail.com>
2025-11-20 10:10:50 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true, required: true },
   password: { type: String },
+  countryCode: { type: String, default: "+1" },
   phone: String,
   
   // --- Profile Fields ---
@@ -20,6 +21,16 @@ const userSchema = new mongoose.Schema({
 
   isAdmin: { type: Boolean, default: false, required: true },
 
-}, { timestamps: true });
+}, { 
+  timestamps: true, 
+  toJSON: { virtuals: true }, // 👈 IMPORTANT: Ensure virtuals show up in JSON responses
+  toObject: { virtuals: true }
+});
+
+// 👇 VIRTUAL: automatic combination when fetching
+userSchema.virtual('fullPhone').get(function() {
+  // Returns combined string like "+919876543210"
+  return `${this.countryCode || ''}${this.phone || ''}`;
+});
 
 module.exports = mongoose.model("User", userSchema);
